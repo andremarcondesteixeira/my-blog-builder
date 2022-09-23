7328a4ed73a3db090b88f4fae2f328d2a4f0f72f
André Teixeira <andremarcondesteixeira@gmail.com>
2022-09-23 12:47:03 -0300

    outlining main logic in a test using comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        // get input path from parameters
+        // get destination folder from parameters
+        // get list of all files to be parsed
+        // parse all files
+        // output result to destination folder
+    }
+}
