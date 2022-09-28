b6a891a2093a1ea6174e0a1779d59b58314a593b
André Teixeira <andremarcondesteixeira@gmail.com>
2022-09-28 17:26:18 -0300

    finished writing integration test (did I?)

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,28 +4,15 @@ use predicates::prelude::*;
 use std::fs;
 
 #[test]
-fn happy_path() -> Result<(), Box<dyn std::error::Error>> {
+fn happy_path() {
     let input = create_input_folder_and_files();
-    let output_folder = create_output_folder();
 
-    // Runs: `./my-blog-builder --input-folder src --output-folder out`
-    let execution_result = execute_program(&input, output_folder);
+    // Runs: `./my-blog-builder --input-folder {input folder path} --output-folder {output folder path}`
+    let execution_result = execute_program(&input);
     execution_result.success().stdout(predicate::eq("Done!"));
 
-    /* Assert that input folder and files are left unchanged */
-    let base_file_content = fs::read_to_string(input.template_file.path()).unwrap();
-    assert_eq!(String::from("Hello #CONTENT"), base_file_content);
-
-    let config_file_content = fs::read_to_string(input.config_file.path()).unwrap();
-    assert_eq!(
-        String::from("extends = \"./base.txt\""),
-        config_file_content
-    );
-
-    let file_before_content = fs::read_to_string(input.file_before.path()).unwrap();
-    assert_eq!(String::from("World!"), file_before_content);
-
-    Ok(())
+    input_folder_and_files_should_be_left_unchanged(&input);
+    output_folder_and_files_should_be_generated_correctly(&input);
 }
 
 struct InputFolderAndFiles {
@@ -79,20 +66,61 @@ fn create_input_file_to_be_processed(
     file_before
 }
 
-fn create_output_folder() -> TempDir {
-    let output_folder = assert_fs::TempDir::new().unwrap();
-    output_folder
-}
-
-fn execute_program(
-    input: &InputFolderAndFiles,
-    output_folder: TempDir,
-) -> assert_cmd::assert::Assert {
+fn execute_program(input: &InputFolderAndFiles) -> assert_cmd::assert::Assert {
     let mut cmd = Command::cargo_bin("my-blog-builder").unwrap();
     cmd.arg("--input-folder")
         .arg(input.input_folder.path())
         .arg("--output-folder")
-        .arg(output_folder.path());
+        .arg("out");
     let execution_result = cmd.assert();
     execution_result
+}
+
+fn input_folder_and_files_should_be_left_unchanged(input: &InputFolderAndFiles) {
+    base_file_should_be_unchanged(input);
+    config_file_should_be_unchanged(input);
+    original_file_should_be_unchanged(input);
+    no_new_files_should_have_been_created_in_the_input_folder(input);
+}
+
+fn base_file_should_be_unchanged(input: &InputFolderAndFiles) {
+    let base_file_content =
+        fs::read_to_string(input.template_file.path()).expect("base file not found");
+    assert_eq!(String::from("Hello #CONTENT"), base_file_content);
+}
+
+fn config_file_should_be_unchanged(input: &InputFolderAndFiles) {
+    let config_file_content =
+        fs::read_to_string(input.config_file.path()).expect("config file not found");
+    assert_eq!(
+        String::from("extends = \"./base.txt\""),
+        config_file_content
+    );
+}
+
+fn original_file_should_be_unchanged(input: &InputFolderAndFiles) {
+    let file_before_content =
+        fs::read_to_string(input.file_before.path()).expect("original file not found");
+    assert_eq!(String::from("World!"), file_before_content);
+}
+
+fn no_new_files_should_have_been_created_in_the_input_folder(input: &InputFolderAndFiles) {
+    let dir_content = fs::read_dir(input.input_folder.path()).unwrap();
+    let amount_of_childs = dir_content.count();
+    assert_eq!(3, amount_of_childs);
+}
+
+fn output_folder_and_files_should_be_generated_correctly(input: &InputFolderAndFiles) {
+    let output_folder = input.input_folder.parent().unwrap().join("out");
+    let output_folder_path = output_folder.as_path();
+
+    assert_eq!(true, output_folder_path.is_dir());
+    assert_eq!(1, fs::read_dir(output_folder_path).unwrap().count());
+
+    let folder_contents = fs::read_dir(output_folder_path).unwrap();
+    let created_file = folder_contents.enumerate().next().unwrap();
+    let created_file_path = (created_file.1).unwrap().path();
+    let created_file_content = fs::read_to_string(created_file_path).unwrap();
+
+    assert_eq!("Hello World!", created_file_content);
 }
